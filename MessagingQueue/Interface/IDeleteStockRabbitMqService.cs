452509af7bffaefcf846786e3a4dde4937081503
Vinoth Ramalingam <274050@cognizant.com>
2022-05-27 13:17:02 +0100

    MessagingQueue WIP version

---
+++ MessagingQueue/Interface/IDeleteStockRabbitMqService.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace EStockCompanyMessagingQueue
+{
+    public interface IDeleteStockRabbitMqService
+    {
+      void SendDeleteCompanyMessage(string deleteCompanyCode);
+    }
+}
--- StockMicroService/Services/StockService.cs
+++ StockMicroService/Services/StockService.cs
@@ -1,20 +1,39 @@
-﻿using StockMicroService.Entities;
+﻿using EStockCompanyMessagingQueue;
+using StockMicroService.Entities;
 using StockMicroService.Models.Stock;
 using StockMicroService.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using VisioForge.Libs.MediaFoundation.OPM;
+using NuGet.Protocol.Plugins;
+using System.Text;
 
 namespace StockMicroService.Services
 {
     public class StockService : IStockService
     {
         private readonly IStockRepository _stockRepository;
-        
-        public StockService(IStockRepository repository)
+        private IConnection _connection;
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionUri;
+        private readonly string _queueName;
+
+        public StockService(IStockRepository repository,IConfiguration configuration)
         {
             this._stockRepository = repository;
+
+            _configuration = configuration;
+
+            var configSection = _configuration.GetSection("RabbitMq");
+
+            _connectionUri = configSection.GetSection("ConnectionUri").Value;
+            _queueName = configSection.GetSection("QueueName").Value;
+
+            CreateConnection();
         }
 
         public StockRequest Create(StockRequest stockRequest)
@@ -22,8 +41,8 @@ namespace StockMicroService.Services
             var stock = new Stock()
             {
                 CompanyCode = stockRequest.CompanyCode,
-                StockPrice = stockRequest.StockPrice,
-                CreatedAt = DateTime.UtcNow,
+                StockPrice = stockRequest.StockPrice,                
+                CreatedAt = DateTime.UtcNow
             };
 
             this._stockRepository.Create(stock);
@@ -50,7 +69,49 @@ namespace StockMicroService.Services
 
         public void Delete(string code)
         {
-            throw new NotImplementedException();
+            if (ConnectionExists())
+            {
+                using (var channel = _connection.CreateModel())
+                {
+                    channel.QueueDeclare(_queueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (sender, e) => {
+                        var body = e.Body.ToArray();
+                        var message = Encoding.UTF8.GetString(body);
+                    };
+                    channel.BasicConsume("_queueName", true, consumer);
+                    _stockRepository.Delete(code);
+                }
+            }
+        }
+
+        private void CreateConnection()
+        {
+            try
+            {
+                var factory = new ConnectionFactory
+                {
+                    Uri = new Uri(_connectionUri)
+                };
+
+                _connection = factory.CreateConnection();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Could not create connection: {ex.Message}");
+            }
+        }
+
+        private bool ConnectionExists()
+        {
+            if (_connection != null)
+            {
+                return true;
+            }
+
+            CreateConnection();
+
+            return _connection != null;
         }
     }
 }
